4d823964f54d5eed58ebd9ac2cff6a613984cb53
Ricardo Quintero <rquintero@itculiacan.edu.mx>
2020-04-02 18:21:12 -0700

    Agregando proyecto ChocolateBoiler...

---
+++ src/main/java/chocolateApp/ChocolateController.java
@@ -0,0 +1,20 @@
+package chocolateApp;
+
+import chocolateBoiler.ChocolateBoiler;
+
+public class ChocolateController {
+	public static void main(String args[]) {
+		ChocolateBoiler boiler = ChocolateBoiler.getInstance();
+		boiler.fill();
+		boiler.boil();
+		boiler.drain();
+
+		// will return the existing instance
+		ChocolateBoiler boiler2 = ChocolateBoiler.getInstance();
+		boiler2.fill();
+		boiler2.boil();
+		boiler2.drain();
+
+
+	}
+}
---
+++ src/main/java/chocolateBoiler/ChocolateBoiler.java
@@ -0,0 +1,54 @@
+package chocolateBoiler;
+ 
+public class ChocolateBoiler {
+	private boolean empty;
+	private boolean boiled;
+	private static ChocolateBoiler uniqueInstance;
+  
+	private ChocolateBoiler() {
+		empty = true;
+		boiled = false;
+	}
+  
+	public static ChocolateBoiler getInstance() {
+		if (uniqueInstance == null) {
+			System.out.println("Creating unique instance of Chocolate Boiler");
+			uniqueInstance = new ChocolateBoiler();
+		}
+		System.out.println("Returning instance of Chocolate Boiler");
+		return uniqueInstance;
+	}
+
+	public void fill() {
+		if (isEmpty()) {
+			empty = false;
+			boiled = false;
+			// fill the boiler with a milk/chocolate mixture
+			System.out.println("Rellenando boiler ...");
+		}
+	}
+ 
+	public void drain() {
+		if (!isEmpty() && isBoiled()) {
+			// drain the boiled milk and chocolate
+			empty = true;
+			System.out.println("Drenando boiler ...");
+		}
+	}
+ 
+	public void boil() {
+		if (!isEmpty() && !isBoiled()) {
+			// bring the contents to a boil
+			boiled = true;
+			System.out.println("Hirviendo boiler ...");
+		}
+	}
+  
+	public boolean isEmpty() {
+		return empty;
+	}
+ 
+	public boolean isBoiled() {
+		return boiled;
+	}
+}
